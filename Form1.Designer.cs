9862ef19b278e5cb9157d944a8a1875178ce7237
Arian <adilsonarian@outlook.com>
2021-09-18 20:38:27 -0600

    Clase Productos añadido y programado

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,20 +31,21 @@ namespace Ejercicio3___Guía3_MM200149
         {
             this.listadoproduct = new System.Windows.Forms.Label();
             this.dtgvlistado = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btneliminar = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dtgvlistado)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -68,19 +69,20 @@ namespace Ejercicio3___Guía3_MM200149
             this.dtgvlistado.Size = new System.Drawing.Size(410, 256);
             this.dtgvlistado.TabIndex = 1;
             // 
-            // button1
+            // btneliminar
             // 
-            this.button1.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(138, 351);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(123, 39);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Eliminar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btneliminar.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btneliminar.Location = new System.Drawing.Point(138, 351);
+            this.btneliminar.Name = "btneliminar";
+            this.btneliminar.Size = new System.Drawing.Size(123, 39);
+            this.btneliminar.TabIndex = 2;
+            this.btneliminar.Text = "Eliminar";
+            this.btneliminar.UseVisualStyleBackColor = true;
             // 
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.Color.LavenderBlush;
+            this.groupBox1.Controls.Add(this.button3);
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.textBox5);
@@ -96,27 +98,88 @@ namespace Ejercicio3___Guía3_MM200149
             this.groupBox1.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(438, 46);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(375, 468);
+            this.groupBox1.Size = new System.Drawing.Size(375, 513);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Mantenimientos de Productos";
             // 
-            // label2
+            // button2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(41, 61);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(76, 26);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Nombre:";
+            this.button2.Location = new System.Drawing.Point(18, 454);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 45);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Guardar Datos";
+            this.button2.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // label1
             // 
-            this.textBox1.Location = new System.Drawing.Point(134, 58);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(206, 34);
-            this.textBox1.TabIndex = 1;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 384);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 26);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Imagen a cargar:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(134, 316);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(103, 34);
+            this.textBox5.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(53, 322);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 26);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Stock:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(134, 263);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(103, 34);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(53, 271);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 26);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Precio:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(134, 203);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(206, 34);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(53, 211);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 26);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Marca:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(134, 121);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(206, 64);
+            this.textBox2.TabIndex = 3;
             // 
             // label3
             // 
@@ -129,91 +192,40 @@ namespace Ejercicio3___Guía3_MM200149
             this.label3.Text = "Descripción:";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
-            // textBox2
+            // textBox1
             // 
-            this.textBox2.Location = new System.Drawing.Point(134, 121);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(206, 64);
-            this.textBox2.TabIndex = 3;
+            this.textBox1.Location = new System.Drawing.Point(134, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(206, 34);
+            this.textBox1.TabIndex = 1;
             // 
-            // label4
+            // label2
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(53, 211);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(64, 26);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Marca:";
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(41, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 26);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nombre:";
             // 
-            // textBox3
+            // button3
             // 
-            this.textBox3.Location = new System.Drawing.Point(134, 203);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(206, 34);
-            this.textBox3.TabIndex = 5;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(53, 271);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(63, 26);
-            this.label5.TabIndex = 6;
-            this.label5.Text = "Precio:";
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(134, 263);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(103, 34);
-            this.textBox4.TabIndex = 7;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(53, 322);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(58, 26);
-            this.label6.TabIndex = 8;
-            this.label6.Text = "Stock:";
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(134, 316);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(103, 34);
-            this.textBox5.TabIndex = 9;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(41, 384);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(144, 26);
-            this.label1.TabIndex = 10;
-            this.label1.Text = "Imagen a cargar:";
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(215, 404);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(154, 45);
-            this.button2.TabIndex = 11;
-            this.button2.Text = "Guardar Datos";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button3.Location = new System.Drawing.Point(186, 454);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(154, 45);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Listado Productos";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(836, 542);
+            this.ClientSize = new System.Drawing.Size(912, 571);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btneliminar);
             this.Controls.Add(this.dtgvlistado);
             this.Controls.Add(this.listadoproduct);
             this.Name = "Form1";
@@ -232,7 +244,7 @@ namespace Ejercicio3___Guía3_MM200149
 
         private System.Windows.Forms.Label listadoproduct;
         private System.Windows.Forms.DataGridView dtgvlistado;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btneliminar;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label3;
@@ -246,6 +258,7 @@ namespace Ejercicio3___Guía3_MM200149
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,11 @@ namespace Ejercicio3___Guía3_MM200149
 {
     public partial class Form1 : Form
     {
+
+        private List<Producto> Productos = new List<Producto>();
+        private int edit_indice = -1;
+
+
         public Form1()
         {
             InitializeComponent();
@@ -19,12 +24,9 @@ namespace Ejercicio3___Guía3_MM200149
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            dtgvlistado.Enabled = false;
-
-
-
-
-
+            dtgvlistado.Visible = false;
+            listadoproduct.Visible = false;
+            btneliminar.Visible = false;
         }
 
         private void label3_Click(object sender, EventArgs e)
@@ -35,6 +37,13 @@ namespace Ejercicio3___Guía3_MM200149
         private void pictureBox1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listadoproduct.Visible = true;
+            dtgvlistado.Visible = true;
+            btneliminar.Visible = true;
         }
     }
 }
